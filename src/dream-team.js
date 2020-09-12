98901b6f70a96f1939b0f7f3f157c194a30645fc
Aliaksei <alex.latypov94@gmail.com>
2020-09-12 17:24:13 +0300

    feat: add task solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) {
+    return false
+};
+  let abbr = [];
+  members.forEach(element => {
+      if(typeof(element) === 'string') {
+          let name = element.replace(/\s/g, "").slice(0, 1).toUpperCase()
+          abbr.push(name)
+      };
+  });
+    return abbr.sort().join('');
 };
